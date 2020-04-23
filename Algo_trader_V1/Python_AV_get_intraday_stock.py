1a368402e6b46d2995d68eda8cc46f923a8d553f
m4Rn1tSCH <44327801+m4Rn1tSCH@users.noreply.github.com>
2020-04-22 23:19:20 -0400

    functions
    -finished basic functions
    -added feature engineering

--- Algo_trader_V1/Python_AV_get_intraday_stock.py
+++ Algo_trader_V1/Python_AV_get_intraday_stock.py
@@ -1,9 +1,87 @@
-from alpha_vantage.timeseries import TimeSeries
-from pprint import pprint
-import pandas as pd
+#OLD WAY IN AV TO PULL DATA
 
-ts = TimeSeries(key='IH4EENERLUFUKJRW', output_format='pandas')
-data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
+#from pprint import pprint
+#import pandas as pd
+#ts = TimeSeries(key='IH4EENERLUFUKJRW', output_format='pandas')
+#data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
 
-data.to_csv('stock_data.csv')
+#data.to_csv('stock_data.csv')
+from Python_alpaca_API_connector import api
 
+def pull_stock_data(symbol, adjusted, outputsize, cadence, output_format):
+    '''
+    DOCUMENTATION
+    symbol: pick abbreviation in letter strings 'XXXX'
+    adjusted: 'True' or 'False'
+    outputsize: 'Full'
+    cadence: 'daily' / 'weekly' / 'monthly'
+    output_format: ['json', 'csv', 'pandas']
+    '''
+    try:
+        df_pull = api.alpha_vantage.historic_quotes(symbol=symbol,
+                                adjusted=adjusted,
+                                outputsize=outputsize,
+                                cadence=cadence,
+                                output_format=output_format)
+    except BaseException as e:
+        print(e)
+        print("API not properly connected")
+    return df_pull
+
+def pull_intraday_data(symbol, interval, outputsize, output_format):
+    '''
+    DOCUMENTATION
+    symbol: pick abbreviation in letter strings 'XXXX'
+    interval: ['1min', '5min', '15min', '30min', '60min']
+    outputsize: 'Full'
+    output_format: ['json', 'csv', 'pandas']
+    '''
+    try:
+        df_intra_pull = api.alpha_vantage.intraday_quotes(symbol=symbol,
+                                        interval=interval,
+                                        outputsize=outputsize,
+                                        output_format=output_format)
+    except BaseException as e:
+        print(e)
+        print("API not properly connected")
+    return df_intra_pull
+
+def submit_order(symbol, qty, side, type, time_in_force, limit_price):
+    '''
+     DOCUMENTATION
+     symbol: Abbr in 'XXX',
+     qty: int,
+     side: 'buy' / 'sell',
+     type: 'limit',
+     time_in_force: 'gtc',
+     limit_price: Any = fl32,
+     stop_price: LIMIT ORDERS DO NOT REQUIRE A STOP PRICE
+     '''
+    try:
+        api.submit_order(symbol=symbol,
+                         qty=qty,
+                         side=side,
+                         type=type,
+                         time_in_force=time_in_force,
+                         limit_price=limit_price
+                         )
+    except BaseException as f:
+        print(f)
+    return 'Order submitted'
+
+# seems to list all equities
+def get_asset_list(status, asset_class):
+    '''
+    Generate a list of all assets
+    status:
+    asset_class:
+    :return:
+    list of asset in possession
+    '''
+    try:
+        asset_list = api.list_assets(status=status,
+                        asset_class=asset_class
+                        )
+    except BaseException as f:
+        print(f)
+    return asset_list
---
+++ Algo_trader_V1/Python_alpaca_API_connector.py
@@ -0,0 +1,86 @@
+# access files
+import alpaca_trade_api as tradeapi
+import acc_config
+from alpha_vantage.timeseries import TimeSeries
+from pprint import pprint
+import pandas as pd
+#initialize the API connection
+api = tradeapi.REST(acc_config.API_KEY,
+                    acc_config.SECRET_KEY,
+                    'https://paper-api.alpaca.markets')
+
+
+
+# Get our account information.
+def acc_report():
+
+    account_info = api.get_account()
+    print(account_info)
+    # Check if our account is restricted from trading.
+    if account_info.trading_blocked:
+        print('Account is currently restricted from trading.')
+    return
+    # Check how much money we can use to open new positions.
+    print('${} is available as buying power.'.format(account.buying_power))
+    return 'acc report:'
+
+# Lists currently open trades
+def list_positions():
+    api = tradeapi.REST(acc_config.API_KEY,
+                        acc_config.SECRET_KEY,
+                        'https://paper-api.alpaca.markets')
+
+    try:
+        positions = api.list_positions()
+        if len(positions) == 0:
+            print("no positions found")
+        else:
+            pass
+    except BaseException as e:
+        print(e)
+    return positions
+
+
+def list_orders():
+
+    try:
+        orders = api.list_orders()
+        if len(orders) == 0:
+            print("no orders found")
+        else:
+            pass
+    except BaseException as e:
+        print(e)
+    return orders
+
+# Places a limit order
+# this one works; order is visible
+#####api.submit_order('AAPL', 1, 'buy', 'limit', 'gtc', 170.50)
+
+def get_acc_profit():
+    if __name__ == '__main__':
+        """
+        With the Alpaca API, you can check on your daily profit or loss by
+        comparing your current balance to yesterday's balance.
+        """
+
+        # Get account info
+        account = api.get_account()
+
+        # Check our current balance vs. our balance at the last market close
+        balance_change = float(account.equity) - float(account.last_equity)
+        print(f'Today\'s portfolio balance change: ${balance_change}')
+
+
+# list orders
+###api.list_orders()
+# Print today's price of a stock
+###api.alpha_vantage.historic_quotes(symbol='AAPL')
+
+# ts = TimeSeries(key='IH4EENERLUFUKJRW', output_format='pandas')
+# data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
+
+# data.to_csv('stock_data.csv')
+
+# Lists all open orders
+# orders = api.list_orders()

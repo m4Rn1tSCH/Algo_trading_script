a649428482c64ca594b81701644253ec8a3e42de
m4Rn1tSCH <44327801+m4Rn1tSCH@users.noreply.github.com>
2020-05-10 15:20:07 -0400

    -added some documentation -removed data_split -split into test and training data is now part of the pipeline function

--- Algo_trader_V1/Python_AI_logic.py
+++ Algo_trader_V1/Python_AI_logic.py
@@ -45,6 +45,86 @@ def set_pipeline_knn(x, y):
     """
     Pipeline - SelectKBest and K Nearest Neighbor
     """
+    df = df
+    label_col = 'open'
+
+    model_features = df.drop(columns=label_col, axis=1, inplace=False)
+    model_label = df[label_col]
+
+    if model_label.dtype == 'float32':
+        model_label = model_label.astype('int32')
+    elif model_label.dtype == 'float64':
+        model_label = model_label.astype('int64')
+    else:
+        print("model label has unsuitable data type!")
+
+    # datetime object is pushed back to be index again before split
+    # this way scaling is possible + index allows plotting later on
+    if df['date'].dtype == 'datetime64[ns]':
+        model_features.set_index('date', drop=True, inplace=True)
+    else:
+        print("datetime object still in df; scaling will fail")
+
+    X_train, X_test, y_train, y_test = train_test_split(model_features,
+                                                        model_label,
+                                                        shuffle=True,
+                                                        test_size=0.4)
+
+    # create a validation set from the training set
+    print(f"Shape of the split training data set X_train:{X_train.shape}")
+    print(f"Shape of the split training data set X_test: {X_test.shape}")
+    print(f"Shape of the split training data set y_train: {y_train.shape}")
+    print(f"Shape of the split training data set y_test: {y_test.shape}")
+
+    # TODO
+    # fit the scaler to the training data first
+    # standard scaler works only with maximum 2 dimensions
+    scaler = StandardScaler(copy=True, with_mean=True, with_std=True).fit(X_train)
+    X_train_scaled = scaler.transform(X_train)
+
+    # transform test data with the object learned from the training data
+    X_test_scaled = scaler.transform(X_test)
+
+    # Principal Component Reduction
+    # keep the most important features of the data
+    pca = PCA(n_components=int(len(df.columns) / 2))
+    # fit PCA model to breast cancer data
+    pca.fit(X_train_scaled)
+    # transform data onto the first two principal components
+    X_train_pca = pca.transform(X_train_scaled)
+    X_test_pca = pca.transform(X_test_scaled)
+    print("Original shape: {}".format(str(X_train_scaled.shape)))
+    print("Reduced shape: {}".format(str(X_train_pca.shape)))
+
+    return list([X_train, X_test, X_train_scaled, X_test_scaled, X_train_pca, X_test_pca])
+
+    # if plot:
+    #     import matplotlib.pyplot as plt
+    #     from sklearn.cluster import KMeans
+    #     '''
+    #                 Plotting of PCA/ Cluster Pairs
+    #
+    #     '''
+    #     #Kmeans clusters to categorize groups WITH SCALED DATA
+    #     #determine number of groups needed or desired for
+    #     kmeans = KMeans(n_clusters=5, random_state=10)
+    #     train_clusters = kmeans.fit(X_train_scaled)
+    #
+    #     kmeans = KMeans(n_clusters=5, random_state=10)
+    #     test_clusters = kmeans.fit(X_test_scaled)
+    #
+    #     fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(12, 10), dpi=600,squeeze=False)
+    #     #styles for title: normal; italic; oblique
+    #     ax[0].scatter(X_train_pca[:, 0], X_train_pca[:, 1], c=train_clusters.labels_)
+    #     ax[0].set_title('Plotted Principal Components of TRAINING DATA', style='oblique')
+    #     ax[0].legend(train_clusters.labels_)
+    #     ax[1].scatter(X_test_pca[:, 0], X_test_pca[:, 1], c=test_clusters.labels_)
+    #     ax[1].set_title('Plotted Principal Components of TEST DATA', style='oblique')
+    #     ax[1].legend(test_clusters.l1abels_)
+    # #principal components of bank panel has better results than card panel with clearer borders
+    # else:
+    #     pass
+
     # Create pipeline with feature selector and classifier
     # replace with gradient boosted at this point or regressor
     pipe = Pipeline([
@@ -74,6 +154,86 @@ def set_pipeline_reg(x, y):
     """
     Pipeline - Logistic Regression and Support Vector Kernel
     """
+
+    df = df
+    label_col = 'open'
+
+    model_features = df.drop(columns=label_col, axis=1, inplace=False)
+    model_label = df[label_col]
+
+    if model_label.dtype == 'float32':
+        model_label = model_label.astype('int32')
+    elif model_label.dtype == 'float64':
+        model_label = model_label.astype('int64')
+    else:
+        print("model label has unsuitable data type!")
+
+    # datetime object is pushed back to be index again before split
+    # this way scaling is possible + index allows plotting later on
+    if df['date'].dtype == 'datetime64[ns]':
+        model_features.set_index('date', drop=True, inplace=True)
+    else:
+        print("datetime object still in df; scaling will fail")
+
+    X_train, X_test, y_train, y_test = train_test_split(model_features,
+                                                        model_label,
+                                                        shuffle=True,
+                                                        test_size=0.4)
+
+    # create a validation set from the training set
+    print(f"Shape of the split training data set X_train:{X_train.shape}")
+    print(f"Shape of the split training data set X_test: {X_test.shape}")
+    print(f"Shape of the split training data set y_train: {y_train.shape}")
+    print(f"Shape of the split training data set y_test: {y_test.shape}")
+
+    # TODO
+    # fit the scaler to the training data first
+    # standard scaler works only with maximum 2 dimensions
+    scaler = StandardScaler(copy=True, with_mean=True, with_std=True).fit(X_train)
+    X_train_scaled = scaler.transform(X_train)
+
+    # transform test data with the object learned from the training data
+    X_test_scaled = scaler.transform(X_test)
+
+    # Principal Component Reduction
+    # keep the most important features of the data
+    pca = PCA(n_components=int(len(df.columns) / 2))
+    # fit PCA model to breast cancer data
+    pca.fit(X_train_scaled)
+    # transform data onto the first two principal components
+    X_train_pca = pca.transform(X_train_scaled)
+    X_test_pca = pca.transform(X_test_scaled)
+    print("Original shape: {}".format(str(X_train_scaled.shape)))
+    print("Reduced shape: {}".format(str(X_train_pca.shape)))
+
+    return list([X_train, X_test, X_train_scaled, X_test_scaled, X_train_pca, X_test_pca])
+
+    # if plot:
+    #     import matplotlib.pyplot as plt
+    #     from sklearn.cluster import KMeans
+    #     '''
+    #                 Plotting of PCA/ Cluster Pairs
+    #
+    #     '''
+    #     #Kmeans clusters to categorize groups WITH SCALED DATA
+    #     #determine number of groups needed or desired for
+    #     kmeans = KMeans(n_clusters=5, random_state=10)
+    #     train_clusters = kmeans.fit(X_train_scaled)
+    #
+    #     kmeans = KMeans(n_clusters=5, random_state=10)
+    #     test_clusters = kmeans.fit(X_test_scaled)
+    #
+    #     fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(12, 10), dpi=600,squeeze=False)
+    #     #styles for title: normal; italic; oblique
+    #     ax[0].scatter(X_train_pca[:, 0], X_train_pca[:, 1], c=train_clusters.labels_)
+    #     ax[0].set_title('Plotted Principal Components of TRAINING DATA', style='oblique')
+    #     ax[0].legend(train_clusters.labels_)
+    #     ax[1].scatter(X_test_pca[:, 0], X_test_pca[:, 1], c=test_clusters.labels_)
+    #     ax[1].set_title('Plotted Principal Components of TEST DATA', style='oblique')
+    #     ax[1].legend(test_clusters.l1abels_)
+    # #principal components of bank panel has better results than card panel with clearer borders
+    # else:
+    #     pass
 
     pipe = Pipeline([
         ('feature_selection', SelectKBest(score_func=chi2)),
@@ -113,6 +273,87 @@ def set_pipeline_rfr():
     Pipeline  - SelectKBest and Random Forest Regressor
     REQUIRES FLOAT32 OR INT32 VALUES AS LABELS
     """
+
+    df = df
+    label_col = 'open'
+
+    model_features = df.drop(columns=label_col, axis=1, inplace=False)
+    model_label = df[label_col]
+
+    if model_label.dtype == 'float32':
+        model_label = model_label.astype('int32')
+    elif model_label.dtype == 'float64':
+        model_label = model_label.astype('int64')
+    else:
+        print("model label has unsuitable data type!")
+
+    # datetime object is pushed back to be index again before split
+    # this way scaling is possible + index allows plotting later on
+    if df['date'].dtype == 'datetime64[ns]':
+        model_features.set_index('date', drop=True, inplace=True)
+    else:
+        print("datetime object still in df; scaling will fail")
+
+    X_train, X_test, y_train, y_test = train_test_split(model_features,
+                                                        model_label,
+                                                        shuffle=True,
+                                                        test_size=0.4)
+
+    # create a validation set from the training set
+    print(f"Shape of the split training data set X_train:{X_train.shape}")
+    print(f"Shape of the split training data set X_test: {X_test.shape}")
+    print(f"Shape of the split training data set y_train: {y_train.shape}")
+    print(f"Shape of the split training data set y_test: {y_test.shape}")
+
+    # TODO
+    # fit the scaler to the training data first
+    # standard scaler works only with maximum 2 dimensions
+    scaler = StandardScaler(copy=True, with_mean=True, with_std=True).fit(X_train)
+    X_train_scaled = scaler.transform(X_train)
+
+    # transform test data with the object learned from the training data
+    X_test_scaled = scaler.transform(X_test)
+
+    # Principal Component Reduction
+    # keep the most important features of the data
+    pca = PCA(n_components=int(len(df.columns) / 2))
+    # fit PCA model to breast cancer data
+    pca.fit(X_train_scaled)
+    # transform data onto the first two principal components
+    X_train_pca = pca.transform(X_train_scaled)
+    X_test_pca = pca.transform(X_test_scaled)
+    print("Original shape: {}".format(str(X_train_scaled.shape)))
+    print("Reduced shape: {}".format(str(X_train_pca.shape)))
+
+    return list([X_train, X_test, X_train_scaled, X_test_scaled, X_train_pca, X_test_pca])
+
+    # if plot:
+    #     import matplotlib.pyplot as plt
+    #     from sklearn.cluster import KMeans
+    #     '''
+    #                 Plotting of PCA/ Cluster Pairs
+    #
+    #     '''
+    #     #Kmeans clusters to categorize groups WITH SCALED DATA
+    #     #determine number of groups needed or desired for
+    #     kmeans = KMeans(n_clusters=5, random_state=10)
+    #     train_clusters = kmeans.fit(X_train_scaled)
+    #
+    #     kmeans = KMeans(n_clusters=5, random_state=10)
+    #     test_clusters = kmeans.fit(X_test_scaled)
+    #
+    #     fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(12, 10), dpi=600,squeeze=False)
+    #     #styles for title: normal; italic; oblique
+    #     ax[0].scatter(X_train_pca[:, 0], X_train_pca[:, 1], c=train_clusters.labels_)
+    #     ax[0].set_title('Plotted Principal Components of TRAINING DATA', style='oblique')
+    #     ax[0].legend(train_clusters.labels_)
+    #     ax[1].scatter(X_test_pca[:, 0], X_test_pca[:, 1], c=test_clusters.labels_)
+    #     ax[1].set_title('Plotted Principal Components of TEST DATA', style='oblique')
+    #     ax[1].legend(test_clusters.l1abels_)
+    # #principal components of bank panel has better results than card panel with clearer borders
+    # else:
+    #     pass
+
     # Create pipeline with feature selector and classifier
     pipe = Pipeline([
         ('feature_selection', SelectKBest(score_func=f_classif)),
@@ -147,6 +388,85 @@ def set_rfe_cross_val(x, y):
         Accuracy: for classification problems
         Mean Squared Error(MSE); Root Mean Squared Error(RSME); R2 Score: for regression
     """
+    df = df
+    label_col = 'open'
+
+    model_features = df.drop(columns=label_col, axis=1, inplace=False)
+    model_label = df[label_col]
+
+    if model_label.dtype == 'float32':
+        model_label = model_label.astype('int32')
+    elif model_label.dtype == 'float64':
+        model_label = model_label.astype('int64')
+    else:
+        print("model label has unsuitable data type!")
+
+    # datetime object is pushed back to be index again before split
+    # this way scaling is possible + index allows plotting later on
+    if df['date'].dtype == 'datetime64[ns]':
+        model_features.set_index('date', drop=True, inplace=True)
+    else:
+        print("datetime object still in df; scaling will fail")
+
+    X_train, X_test, y_train, y_test = train_test_split(model_features,
+                                                        model_label,
+                                                        shuffle=True,
+                                                        test_size=0.4)
+
+    # create a validation set from the training set
+    print(f"Shape of the split training data set X_train:{X_train.shape}")
+    print(f"Shape of the split training data set X_test: {X_test.shape}")
+    print(f"Shape of the split training data set y_train: {y_train.shape}")
+    print(f"Shape of the split training data set y_test: {y_test.shape}")
+
+    # TODO
+    # fit the scaler to the training data first
+    # standard scaler works only with maximum 2 dimensions
+    scaler = StandardScaler(copy=True, with_mean=True, with_std=True).fit(X_train)
+    X_train_scaled = scaler.transform(X_train)
+
+    # transform test data with the object learned from the training data
+    X_test_scaled = scaler.transform(X_test)
+
+    # Principal Component Reduction
+    # keep the most important features of the data
+    pca = PCA(n_components=int(len(df.columns) / 2))
+    # fit PCA model to breast cancer data
+    pca.fit(X_train_scaled)
+    # transform data onto the first two principal components
+    X_train_pca = pca.transform(X_train_scaled)
+    X_test_pca = pca.transform(X_test_scaled)
+    print("Original shape: {}".format(str(X_train_scaled.shape)))
+    print("Reduced shape: {}".format(str(X_train_pca.shape)))
+
+    return list([X_train, X_test, X_train_scaled, X_test_scaled, X_train_pca, X_test_pca])
+
+    # if plot:
+    #     import matplotlib.pyplot as plt
+    #     from sklearn.cluster import KMeans
+    #     '''
+    #                 Plotting of PCA/ Cluster Pairs
+    #
+    #     '''
+    #     #Kmeans clusters to categorize groups WITH SCALED DATA
+    #     #determine number of groups needed or desired for
+    #     kmeans = KMeans(n_clusters=5, random_state=10)
+    #     train_clusters = kmeans.fit(X_train_scaled)
+    #
+    #     kmeans = KMeans(n_clusters=5, random_state=10)
+    #     test_clusters = kmeans.fit(X_test_scaled)
+    #
+    #     fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(12, 10), dpi=600,squeeze=False)
+    #     #styles for title: normal; italic; oblique
+    #     ax[0].scatter(X_train_pca[:, 0], X_train_pca[:, 1], c=train_clusters.labels_)
+    #     ax[0].set_title('Plotted Principal Components of TRAINING DATA', style='oblique')
+    #     ax[0].legend(train_clusters.labels_)
+    #     ax[1].scatter(X_test_pca[:, 0], X_test_pca[:, 1], c=test_clusters.labels_)
+    #     ax[1].set_title('Plotted Principal Components of TEST DATA', style='oblique')
+    #     ax[1].legend(test_clusters.l1abels_)
+    # #principal components of bank panel has better results than card panel with clearer borders
+    # else:
+    #     pass
 
     # Use the Cross-Validation function of the RFE module
     # LOGISTIC REGRESSION

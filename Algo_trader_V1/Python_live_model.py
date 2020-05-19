86a9beaafdf6aac6cb6ac9863952e3cf32ad75b2
m4Rn1tSCH <44327801+m4Rn1tSCH@users.noreply.github.com>
2020-05-19 08:48:15 -0400

    simple loop
    -preparatory loop for live model

--- Algo_trader_V1/Python_live_model.py
+++ Algo_trader_V1/Python_live_model.py
@@ -4,7 +4,8 @@ Created on 18 5/18/2020 8:15 PM 2020
 
 @author: bill-
 """
-
+from datetime import datetime as dt
+import time
 """
 -pull data every hour /every day
 -prepare df and decide what to sell or buy
@@ -12,3 +13,14 @@ if loop -> buy or sell
 append data every hour
 calculate/or mark -> sell or buy order
 """
+# TODO
+äloop works
+timestamp = dt.timestamp(dt.today())
+def test():
+    while True:
+        print("running")
+        time.sleep(5)
+
+
+
+

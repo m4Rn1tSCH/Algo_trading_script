d4508493fccd3f9535bfb771143069655efb8669
Billateral <bill@envel.ai>
2020-07-23 18:15:56 -0400

    --restructured into separate folders  --Introduced second loop with a simple moving average  Model:     -fixed working directory bug     -minor spelling     -weighted moving average     -docs changed for submit order     -streamlined imports     -correct evaluation of signal (intersection 50/200)     -list exhaustion handling when iterating over dates     - added buy signal and sell signal Raw Code Snippets     -raw code goes here that might prove useful later on  AV_intraday     -fixed plotting feature and added to df pull/intra_df_pull     -plots are not co-existing and not overwriting each other

---
+++ Algo_trader_V1/starter_app.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 23 7/23/2020 5:46 PM
+
+@author: bill-
+"""
+'''
+This endpoint unifies all functions
+and starts the algorithm.
+Specify which loop and stock
+'''

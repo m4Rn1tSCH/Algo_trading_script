e30d1d2bf292b69a181faede7141695cd4dd4c56
Bill_Jaenke <bjaenke2017@student.hult.edu>
2020-11-11 14:47:47 -0500

    11/11/2020 -minor wording changes for overview during execution -refactor to handle closed markets and avoid crashing -double loop with handling and sleep time until next open tested

--- Algo_trader_V1/unfinished_scripts/test_ground.py
+++ Algo_trader_V1/unfinished_scripts/test_ground.py
@@ -16,7 +16,10 @@ from Algo_trader_V1.api.alpaca_API_connector import api
 stocks = ['BILL', 'CUK', 'AAPL', 'NVDA']
 # outer infinite loop with inner check
 while True:
-    while api.get_clock().timestamp > api.get_clock().next_open:
-        # if api.get_clock().timestamp < api.get_clock().next_close and api.get_clock().timestamp > api.get_clock().next_open:
+    while api.get_clock().timestamp < api.get_clock().next_close and \
+            api.get_clock().timestamp > api.get_clock().next_open:
         print("open")
-    print("closed")
+    print("Markets closed at: ", api.get_clock().next_close, "Algo is inactive for next: ",\
+          (api.get_clock().next_open - api.get_clock().timestamp).seconds, "s")
+    time.sleep((api.get_clock().next_open - api.get_clock().timestamp).seconds)
+

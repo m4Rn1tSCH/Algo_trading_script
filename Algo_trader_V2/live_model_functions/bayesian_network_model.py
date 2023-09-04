99036af6c2bd76a4f9fd4d5db97bfc930d933fc4
m4Rn1tSCH <bjaenke2017@student.hult.edu>
2023-09-04 11:54:37 -0400

    09/04/2023 -preparation for bayesian network build

---
+++ Algo_trader_V2/live_model_functions/bayesian_network_model.py
@@ -0,0 +1,10 @@
+"""
+Created on: 9/4/2023; 11:52 AM
+
+Created by: Bill J
+"""
+# TODO: build bayes network with nodes
+# resources:
+# http://www.stat.yale.edu/Courses/1997-98/101/condprob.htm
+# C:/Users/Bill J/OneDrive/Docs Bill/TA_files/Python_ML_AI_mat/Market_analysis_and_trading_strat_with_bayesian_networks.pdf
+# install pymc
